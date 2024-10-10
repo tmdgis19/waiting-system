69bf7369f3f768f6be477eb2805fae67858b143e
seungheon <seungheon@apexsoft.co.kr>
2024-10-11 00:32:58 +0900

    README 파일 작성 중 - Reactive Stream

---
+++ README.md
@@ -0,0 +1,16 @@
+## Web Flux
+### Reactive Streams
+> Reactive Streams is an initiative to provide a standard 
+> for asynchronous stream processing with non-blocking back pressure.
+
+Reactive Streams의 공식 사이트에 있는 문장이다. 이보다 명확하게 설명하기는 어렵기 때문에 인용했다.
+해석하자면 Reactive Streams는 비차단 백프레셔(역압)를 처리하기 위 비동기 스트림 표준이란 뜻이다.
+
+영어를 직역하자니 조금 어색한데, 핵심 키워드는 Non-Blocking, Back Pressure, 비동기 스트림이라고 생각한다.
+
+## Redis 
+
+## 주요 API 개요
+
+## 참고 자료
+https://engineering.linecorp.com/ko/blog/reactive-streams-with-armeria-1
--- waiting-api/src/test/java/com/personal/waiting_api/FlowApplicationTests.java
+++ waiting-api/src/test/java/com/personal/waiting_api/FlowApplicationTests.java
@@ -1,13 +1,20 @@
 package com.personal.waiting_api;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Operators;
+import reactor.core.publisher.Operators.MonoSubscriber;
 
-@SpringBootTest
+//@SpringBootTest
 class FlowApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Flux.just(1,2,3,4)
+			.doOnNext(it-> System.out.println(it))
+			.subscribe();
 	}
 
 }
